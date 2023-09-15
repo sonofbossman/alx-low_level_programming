2be45caedbde05d08309ca7c206d760b4615ce54
sonofbossman <akingbadeeniola452@gmail.com>
2023-09-15 15:00:00 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,14 +9,17 @@ int main(void)
 
 for (i = 1; i <= 100; i++)
 {
-	if (i % 3 == 0)
-		printf("Fizz ");
-	else if (i % 5 == 0)
-		printf("Buzz ");
-	else if ((i % 3 == 0) && (i % 5 == 0))
-		printf("FizzBuzz ");
+	if ((i % 3) == 0)
+		printf("Fizz");
+	else if ((i % 5) == 0)
+		printf("Buzz");
+	else if ((i % 3) == 0 && (i % 5) == 0)
+		printf("FizzBuzz");
 	else
-		printf("%d ", i);
+		printf("%d", i);
+	if (i == 100)
+		continue;
+	printf(" ");
 }
 printf("\n");
 
