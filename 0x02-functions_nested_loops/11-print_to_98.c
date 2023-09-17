039870ca1e584dce34d8bebfaa5cea3abd0572dd
sonofbossman <akingbadeeniola452@gmail.com>
2023-09-17 08:38:18 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,11 +9,27 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i <= 98; i++)
+	if (!(n > 98))
 	{
-		printf("%d", i);
-		if (i != 98)
-			printf(", ");
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
 	}
 	printf("\n");
 }
