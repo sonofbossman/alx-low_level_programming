673b294240b24a72fd8a0228e45faeb099eed09a
sonofbossman <akingbadeeniola452@gmail.com>
2023-10-02 02:44:13 -0700

    Create the static library libmy.a containing all the functions listed below:

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: character to be checked
+ * Return: 1 if lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_strcpy - Write a function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ * @dest: pointer one
+ * @src: pointer two
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
