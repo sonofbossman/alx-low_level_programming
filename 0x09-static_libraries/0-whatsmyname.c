9bb7a40d517beccc82f4ca6648c3f49779840052
sonofbossman <akingbadeeniola452@gmail.com>
2023-10-02 05:03:25 -0700

    Write a program that prints its name, followed by a new line

---
+++ 0x09-static_libraries/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry of the program
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
